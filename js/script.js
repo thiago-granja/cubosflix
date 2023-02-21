3e87a147af1d05911b911c52763e307ee83f40e1
Thiago Granja <thiagoaagranja@outlook.com>
2023-02-21 19:24:29 -0300

    feat: added carousel swipe functionality

--- js/script.js
+++ js/script.js
@@ -4,13 +4,16 @@ const themeBtn = document.querySelector('.btn-theme');
 const previousBtn = document.querySelector('.btn-prev');
 const nextBtn = document.querySelector('.btn-next');
 const input = document.querySelector('.input');
-const movies = document.querySelector('.movies');
+const carouselDisplay = document.querySelector('.movies-container');
+let carouselPages = document.querySelectorAll('.movies');
 const modal = document.querySelector('.modal');
 const closeBtn = document.querySelector('.modal__close');
+const PAGE_QTY = 3;
+const MOVIES_PER_PAGE = 6;
 let currentPage = 0;
 let nextPage = 1;
 let previousPage = 2;
-let movieList = [[], [], []];
+let movieList = [];
 const darkMode = {
     '--background': '#1B2028',
     '--bg-secondary': '#2D3440',
@@ -31,97 +34,91 @@ const lightMode =  {
 };
 
 pickTheme();
-spawnMovies();
 startCarousel();
 highlightMovie(436969);
 
 
-themeBtn.addEventListener('click', changeTheme);
-
-modal.addEventListener('click', () => {
-    modal.classList.toggle("hidden");
-})
-
 previousBtn.addEventListener('click', () => {
-    changeMovies('left');
+    (currentPage === 0 ? currentPage = 2 : currentPage--)
+    swipe();
 })
 
 nextBtn.addEventListener('click', () => {
-    changeMovies('right');
+    (currentPage === 2 ? currentPage = 0 : currentPage++)
+    swipe();
 })
 
 input.addEventListener('keydown', (event) => {  
     if (event.key != 'Enter' && event.key != 'NumpadEnter') return;
     currentPage = 0;
-    if (input.value) {
-        startCarousel(`${input.value}`);
-        input.value = "";
-    } else {
-        startCarousel();
-    }
 
+    if (input.value) updateDisplay(`${input.value}`);
+    else updateDisplay();
+
+    input.value = "";
+    swipe();
 })
 
-movies.addEventListener('click', async (event) => {
+carouselDisplay.addEventListener('click', async (event) => {
+    if (event.target.classList != 'movie') return;
     const modalMovie = await fetchMovie(event.target.id);
     showModal(modalMovie);
 })
 
-function pickTheme() {
-    let theme = localStorage.getItem('theme');
-    if (!theme) theme = 'light';
+modal.addEventListener('click', () => {
+    modal.classList.toggle("hidden");
+})
 
-    if (theme === 'dark') {
-        for (key in darkMode) root.style.setProperty(`${key}`, `${darkMode[key]}`);
-        logo.src = `./assets/${theme}-mode/logo.svg`;
-    } else {
-        for (key in lightMode) root.style.setProperty(`${key}`, `${lightMode[key]}`);
-        logo.src = `./assets/${theme}-mode/logo.png`;
-    }
+themeBtn.addEventListener('click', changeTheme);
 
-    themeBtn.src = `./assets/${theme}-mode/theme.svg`;
-    previousBtn.src = `./assets/${theme}-mode/arrow-left.svg`;
-    nextBtn.src = `./assets/${theme}-mode/arrow-right.svg`;
-    closeBtn.src = `./assets/${theme}-mode/close.svg`;
-}
-
-function changeTheme() {
-    let theme = localStorage.getItem('theme');
-    if (!theme || theme === 'light' ? theme = 'dark' : theme = 'light');
-    localStorage.setItem('theme', theme);
-    pickTheme();
-}
-
-function spawnMovies() {
-    for (let i = 0; i < 6; i++){
-        const newMovie = document.createElement('div');
-        const info = document.createElement('div');
-        const title = document.createElement('span');
-        const rating = document.createElement('span');
-        const star = document.createElement('img');
-        newMovie.classList = 'movie';
-        info.classList = 'movie__info';
-        title.classList = 'movie__title';
-        rating.classList = 'movie__rating';
+async function getMovies(query) {
+    movieList = [];
+    let response;
     
-        newMovie.appendChild(info);
-        info.appendChild(title);
-        info.appendChild(rating);
-        rating.appendChild(star);
-        movies.appendChild(newMovie);
-    }
+    if (!query) response = await api.get('/3/discover/movie?language=pt-BR&include_adult=false'); 
+    else response = await api.get(`/3/search/movie?language=pt-BR&include_adult=false&query=${query}`);
+
+    for (let i = 0; i < 18; i++) movieList.push(response.data.results[i]);
 }
 
-function changeMovies(direction) {
-    const displayed = document.querySelectorAll('.movie');
-    
-    if (direction === 'right') (currentPage === 2 ? currentPage = 0 : currentPage++);
-    else if (direction === 'left') (currentPage === 0 ? currentPage = 2 : currentPage--);
-    const moviesToSpawn = movieList[currentPage];
+function spawnStructure() {
+    for (let j = 0; j < PAGE_QTY; j++){
+        const page = document.createElement('div');
+        page.classList = 'movies';
+        if (j === 0) page.classList.add('active');
+        if (j === 1) page.classList.add('next');
+        if (j === 2) page.classList.add('previous');
+        
+        for (let i = 0; i < MOVIES_PER_PAGE; i++){
+            const newMovie = document.createElement('div');
+            const info = document.createElement('div');
+            const title = document.createElement('span');
+            const rating = document.createElement('span');
+            const star = document.createElement('img');
+            newMovie.classList = 'movie';
+            info.classList = 'movie__info';
+            title.classList = 'movie__title';
+            rating.classList = 'movie__rating';
+            
+            newMovie.appendChild(info);
+            info.appendChild(title);
+            info.appendChild(rating);
+            rating.appendChild(star);
+            page.appendChild(newMovie);
+        }
 
-    for (let i = 0; i < moviesToSpawn.length; i++){
-        const newMovie = moviesToSpawn[i];
-        const movie = displayed[i];
+        carouselDisplay.insertBefore(page, nextBtn);
+    }
+    carouselPages = document.querySelectorAll('.movies');
+}
+
+async function updateDisplay(query) {
+    if (!query ? await getMovies(false) : await getMovies(query));
+    movies = document.querySelectorAll('.movie');
+    
+    for (let i = 0; i < movieList.length; i++){
+        const newMovie = movieList[i];
+        const movie = movies[i];
         
         const title = movie.querySelector(`.movie__title`);
         const rating = movie.querySelector(`.movie__rating`);
@@ -133,23 +130,10 @@ function changeMovies(direction) {
     }
 }
 
-async function getMovies(query) {
-    movieList = [[], [], []];
-    let response;
-    
-    if (!query) response = await api.get('/3/discover/movie?language=pt-BR&include_adult=false'); 
-    else response = await api.get(`/3/search/movie?language=pt-BR&include_adult=false&query=${query}`);
-
-    for (let i = 0; i < 18; i++){
-        if (i < 6) movieList[0].push(response.data.results[i]);
-        else if (i < 12) movieList[1].push(response.data.results[i]);
-        else movieList[2].push(response.data.results[i]);
-    }
-}
-
 async function startCarousel(query) {
-    if (!query ? await getMovies(false) : await getMovies(query));
-    changeMovies();
+    await getMovies();
+    await spawnStructure();
+    updateDisplay();
 }
 
 async function highlightMovie(id) {
@@ -167,7 +151,7 @@ async function highlightMovie(id) {
     const highlightLaunch = document.querySelector('.highlight__launch');
     const highlightDescription = document.querySelector('.highlight__description');    
 
-    highlightLink.href = `https://www.youtube.com/watch?v=${video.data.results[1].key}`; // trailer fica no 1, instruções mandam pegar o 0
+    highlightLink.href = `https://www.youtube.com/watch?v=${video.data.results[1].key}`;
     highlightVideo.style.backgroundImage = `url('${response.backdrop_path}')`;
     highlightTitle.innerHTML = `${response.title}`;
     highlightRating.innerHTML = `${response.vote_average.toFixed(1)}`;
@@ -197,16 +181,48 @@ function showModal(movie){
         genres.appendChild(newGenre);
     }
     
-
     title.innerHTML = movie.title;
     img.src = movie.backdrop_path;
     description.innerHTML = movie.overview;
     average.innerHTML = movie.vote_average.toFixed(1);
-
-
     modal.classList.toggle("hidden");
 }
 
-//create 3 .movies containers (1 on html already);
-//spawn 6 movies on each (whole list);
-//apply 0 opacity to previous and next pages and 1 on active page;
+function direction() {
+    nextPage = ((currentPage + 1) > 2 ? nextPage = 0 : nextPage = currentPage + 1)
+    previousPage = ((currentPage - 1) < 0 ? previousPage = 2 : previousPage = currentPage - 1)
+}
+
+function swipe() {
+    direction();
+    carouselPages[currentPage].classList = 'movies active';
+    carouselPages[nextPage].classList = 'movies next';
+    carouselPages[previousPage].classList = 'movies previous';
+}
+
+function pickTheme() {
+    let theme = localStorage.getItem('theme');
+    if (!theme) theme = 'light';
+
+    if (theme === 'dark') {
+        for (key in darkMode) root.style.setProperty(`${key}`, `${darkMode[key]}`);
+        logo.src = `./assets/${theme}-mode/logo.svg`;
+    } else {
+        for (key in lightMode) root.style.setProperty(`${key}`, `${lightMode[key]}`);
+        logo.src = `./assets/${theme}-mode/logo.png`;
+    }
+
+    themeBtn.src = `./assets/${theme}-mode/theme.svg`;
+    previousBtn.src = `./assets/${theme}-mode/arrow-left.svg`;
+    nextBtn.src = `./assets/${theme}-mode/arrow-right.svg`;
+    closeBtn.src = `./assets/${theme}-mode/close.svg`;
+}
+
+function changeTheme() {
+    let theme = localStorage.getItem('theme');
+    if (!theme || theme === 'light' ? theme = 'dark' : theme = 'light');
+    localStorage.setItem('theme', theme);
+    pickTheme();
+}
+
+// checar casos de try catch - quando procura matrix, 1 deles falha em trazer a imagem. colocar UNAVAILABLE no lugar da imagem do poster?
